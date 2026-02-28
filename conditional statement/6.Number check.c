d21776a2dcf1ad5127dba1a8b503f6c2b42fecaa
kabil-77 <kabil200613@gmail.com>
2026-02-28 14:16:07 +0530

    Add files via upload

---
+++ conditional statement/6.Number check.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int num;
+    scanf("%d",&num);
+    if(num==0){
+        printf("Zero");
+    }
+    else if(num<0){
+        printf("Negative");
+    }
+    else{
+        printf("Positive");
+    }
+}
